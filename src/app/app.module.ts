26d07c9794f1f93e3ed07a17c322d607c08fbf4a
Maritza Loaiza Urrutia <mloaizau@everis.com>
2020-07-19 13:44:28 -0400

    se agrega el componente grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,8 @@ import { AppComponent } from "./app.component";
 // Uncomment and add to NgModule imports if you need to use two-way binding
 import { NativeScriptFormsModule } from "@nativescript/angular/forms";
 import { StackComponent } from './layouts/stack/stack.component';
-import { FlexboxComponent } from './layouts/flexbox/flexbox.component'
+import { FlexboxComponent } from './layouts/flexbox/flexbox.component';
+import { GridComponent } from './layouts/grid/grid.component'
 
 // Uncomment and add to NgModule imports if you need to use the HttpClient wrapper
 // import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
@@ -21,7 +22,8 @@ import { FlexboxComponent } from './layouts/flexbox/flexbox.component'
         AppComponent,
         CurrentChallengeComponent,
         StackComponent,
-        FlexboxComponent
+        FlexboxComponent,
+        GridComponent
     ],
     bootstrap: [
         AppComponent
