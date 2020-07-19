9cc90ad11afadf6251bacd5db3ba38e8d27a8238
Maritza Loaiza Urrutia <mloaizau@everis.com>
2020-07-19 13:00:21 -0400

    se agrega el componente flexbox

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,8 @@ import { AppComponent } from "./app.component";
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 import { NativeScriptFormsModule } from "@nativescript/angular/forms";
-import { StackComponent } from './layouts/stack/stack.component'
+import { StackComponent } from './layouts/stack/stack.component';
+import { FlexboxComponent } from './layouts/flexbox/flexbox.component'
 
 // Uncomment and add to NgModule imports if you need to use the HttpClient wrapper
 // import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
@@ -19,7 +20,8 @@ import { StackComponent } from './layouts/stack/stack.component'
     declarations: [
         AppComponent,
         CurrentChallengeComponent,
-        StackComponent
+        StackComponent,
+        FlexboxComponent
     ],
     bootstrap: [
         AppComponent
