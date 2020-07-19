9e5f1941d33254069f81a3e899eb99e6bfdaf1e1
Maritza Loaiza Urrutia <mloaizau@everis.com>
2020-07-19 12:44:15 -0400

    subida del proyecto

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NativeScriptModule } from "@nativescript/angular";
+
+import { CurrentChallengeComponent } from "./Challenges/current-challenge/current-challenge.component"
+import { AppComponent } from "./app.component";
+
+// Uncomment and add to NgModule imports if you need to use two-way binding
+import { NativeScriptFormsModule } from "@nativescript/angular/forms";
+import { StackComponent } from './layouts/stack/stack.component'
+
+// Uncomment and add to NgModule imports if you need to use the HttpClient wrapper
+// import { NativeScriptHttpClientModule } from "nativescript-angular/http-client";
+
+@NgModule({
+    imports: [
+        NativeScriptModule,
+        NativeScriptFormsModule
+    ],
+    declarations: [
+        AppComponent,
+        CurrentChallengeComponent,
+        StackComponent
+    ],
+    bootstrap: [
+        AppComponent
+    ],
+    providers: [],
+    schemas: [
+        NO_ERRORS_SCHEMA
+    ]
+})
+/*
+Pass your application module to the bootstrapModule function located in main.ts to start your app
+*/
+export class AppModule { }
