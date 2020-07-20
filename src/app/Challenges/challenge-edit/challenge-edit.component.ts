4dfd902aebdbdb18dab8adb5afd0b29e12d9ddb2
Maritza Loaiza Urrutia <mloaizau@everis.com>
2020-07-19 22:46:08 -0400

    pasar datos entre componentes

--- src/app/Challenges/challenge-edit/challenge-edit.component.ts
+++ src/app/Challenges/challenge-edit/challenge-edit.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'ns-challenge-edit',
   templateUrl: './challenge-edit.component.html',
   styleUrls: ['./challenge-edit.component.css']
 })
-export class ChallengeEditComponent implements OnInit {
+export class ChallengeEditComponent {
+    @Output() input = new EventEmitter<string>();
+    challengeDescription = "";
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+    onSetChallenge(){
+        // this.currentChallenge = this.challengeDescription;
+        this.input.emit(this.challengeDescription)
+    }
 
 }
