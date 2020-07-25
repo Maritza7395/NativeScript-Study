10ac3f57638cf52050588f3f1d1693636cacc1a2
Maritza Loaiza Urrutia <mloaizau@everis.com>
2020-07-25 18:10:30 -0400

    barra de accion y boton atras

--- src/app/Challenges/current-challenge/current-challenge.component.ts
+++ src/app/Challenges/current-challenge/current-challenge.component.ts
@@ -1,6 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { ItemEventData } from 'tns-core-modules/ui/list-view';
+import { RouterExtensions } from '@nativescript/angular/router';
+import { isAndroid } from 'tns-core-modules/platform'
+import { Page } from 'tns-core-modules/ui/page'
 
+declare var android: any;
 @Component({
     selector: 'ns-current-challenge',
     templateUrl: './current-challenge.component.html',
@@ -9,7 +13,27 @@ import { ItemEventData } from 'tns-core-modules/ui/list-view';
 })
 
 export class CurrentChallengeComponent {
+
+    constructor(
+        private router: RouterExtensions,
+        private page: Page
+        ){}
+
     @Input() challengeTitle = '';
+
+    onEdit(){
+        this.router.navigate(['/edit-challenge'])
+    }
+
+    onLoadedActionBar(){
+        if(isAndroid){
+            const androidToolbar = this.page.actionBar.nativeView;
+            const backButton = androidToolbar.getNavigationIcon();
+            if(backButton){
+                backButton.setColorFilter(android.graphics.Color.parseColor('#171717'), (<any>android.graphics).PorterDuff.Mode.SRC_ATOP);
+            }
+        }
+    }
 
     // onItemTap(args: ItemEventData){
     //     console.log(args)
